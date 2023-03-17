cc06a6e7762f847b9a701b7f67c8db2be58b4efa
root <tshegofatsoseane98@gmail.com>
2023-03-17 14:54:25 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,14 +9,32 @@ int main(void)
 int nums;
 int nums1;
 
-for (nums = '0'; nums <= '8'; nums++)
+for (nums = '0'; nums < '9'; nums++)
 {
-for (nums1 = '1'; nums1 <= '9'; nums1++)
+for (nums1 = '0'; nums1 <= '9'; nums1++)
 {
+if (nums == '0' && nums1 < '1')
+continue;
+if (nums == '1' && nums1 < '2')
+continue;
+if (nums == '2' && nums1 < '3')
+continue;
+if (nums == '3' && nums1 < '4')
+continue;
+if (nums == '4' && nums1 < '5')
+continue;
+if (nums == '5' && nums1 < '6')
+continue;
+if (nums == '6' && nums1 < '7')
+continue;
+if (nums == '7' && nums1 < '8')
+continue;
+if (nums == '8' && nums1 < '9')
+continue;
 putchar(nums);
 putchar(nums1);
-if (nums == '9' && nums1 == '9')
-continue;
+if (nums == '8' && nums1 == '9')
+break;
 putchar(',');
 putchar(' ');
 }
