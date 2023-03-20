7bfed98f3ba2daed91bc720148f2574b9bc5fb2b
root <tshegofatsoseane98@gmail.com>
2023-03-20 12:17:04 -0700

    Task 0, a program that prints _putchar, followed by a new line.

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Prints "Holberton" followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	_putchar('_putchar');
+	_putchar('\n');
+	return (0);
+}
