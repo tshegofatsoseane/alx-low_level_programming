8b7533e6a3fbe87300fa5abbbba32bd02cd7d464
root <tshegofatsoseane98@gmail.com>
2023-03-21 11:46:22 -0700

    A function that prints the sign of a number.
    :wq
    clear
    wq
    :wq
    unction that prints the sign of a number.
    
    git add .
    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 5-main.c 5-sign.c -o 5-sign
    betty  5-sign.c
    exit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,16 +1,27 @@
 #include "main.h"
 
 /**
- * _abs - computes the absolute value of an integer
- * @n: the int to check
- * Return: the absolute value of int
+ *  print_sign - check input if its positive, negative or zero
+ * @n: variable to determine if input is greater than, less than or is zero
+ * Return: 0 if succesfu
  */
-int _abs(int n)
+int print_sign(int n)
 {
-	if (n >= 0)
-	{
-		return (n);
-	}
-	return (-n);
+int i;
+if (n > 0)
+{
+_putchar('+');
+i = 1;
 }
-
+else if (n < 0)
+{
+_putchar('-');
+i = -1;
+}
+else
+{
+_putchar('0');
+i = 0;
+}
+return (i);
+}
