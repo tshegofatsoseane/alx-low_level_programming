9369120a244ac389a0843b20c64e928ff6114578
root <tshegofatsoseane98@gmail.com>
2023-04-03 19:44:48 -0700

    Task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strchr - Entry point
+ * @s: input
+ * @c: input
+ * Return: Always 0 (Success)
+ */
+char *_strchr(char *s, char c)
+{
+    while (*s != '\0') 
+    {
+        if (*s == c) 
+	{
+            return s;
+        }
+        s++;
+    }
+    if (*s == c)
+    {
+        return s;
+    }
+    return (NULL);
+}
