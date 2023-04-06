a8e15ee1a70ca23daff47cc569ddf353dbf5184b
root <tshegofatsoseane98@gmail.com>
2023-04-06 14:12:22 -0700

    Task 100

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * is_palindrome - checks to see if a string is a palindrome
+ * @s: pointer to input string
+ */
+int is_palindrome(char *s)
+{
+	if (*s != '\0' || *(s + 1) != '\0')
+	{
+		return (-1);
+	}
+	if (*s == '\0' || *(s + 1) == '\0')
+	{
+		return (1);
+	}
+
+	if (*s == *(s + strlen(s) - 1))
+	{
+		return (is_palindrome(s + 1) && is_palindrome(s + strlen(s) - 1));
+	}
+
+	return (0);
+}
