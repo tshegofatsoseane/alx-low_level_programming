b173d41b57a594f5b61f9cb5d6b7bfba6e7c4bae
root <tshegofatsoseane98@gmail.com>
2023-04-10 17:08:13 -0700

    Task 0

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * _isupper - check capital letters
+ * @c: char to check
+ * Return: 0 or 1
+ */
+int _isupper(int c)
+{
+}
---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * char *_strcpy - a function that copies the string pointed to by source
+ * @dest: copy destinantion
+ * @src: copy source
+ * Return: string
+ */
+char *_strcpy(char *dest, char *src)
+{
+}
