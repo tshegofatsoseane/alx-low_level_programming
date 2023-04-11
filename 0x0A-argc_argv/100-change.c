f72e149f9d4a17111ca01cea495a41f396286464
root <tshegofatsoseane98@gmail.com>
2023-04-10 18:59:19 -0700

    Task 5

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - prints the no of coins to make change
+ * @argc: no of args
+ * @argv: array of args
+ * Return: 0 (Success) and for 1 (Error)
+ */
+
+int main(int argc, char *argv[])
+{
+	int coins = 0;
+	int cents;
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	 cents = atoi(argv[1]);
+
+	if (cents < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+
+	coins += cents / 25;
+	cents %= 25;
+
+	coins += cents / 10;
+	cents %= 10;
+
+	coins += cents / 5;
+	cents %= 5;
+
+	coins += cents / 2;
+	cents %= 2;
+
+	coins += cents;
+	printf("%d\n", coins);
+	return (0);
+}
