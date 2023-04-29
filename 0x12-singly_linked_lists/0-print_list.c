d47d5fe52a3044b63889f8ed759c3ce3718e3de1
tshegofatsoseane <tshegofatsoseane98@gmail.com>
2023-04-28 20:04:22 -0700

    Task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,21 +11,22 @@
 
 size_t print_list(const list_t *h)
 {
-
 	size_t count = 0;
-	int counter;
 
-	while (h != NULL)
-	if (h->str == NULL)
+	while (h)
 	{
-		printf("[%d] (nil)\n", h->len);
-	}
-	else
-	{
-		printf("[%d] %s\n", h->len, h->str);
+		if (!h->str)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%u] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		count++;
 	}
 
-	count++;
-	h = h->next;
 	return (count);
+
 }
