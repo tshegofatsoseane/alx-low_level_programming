4bf08c02cb3ba2098234a4975ef62ce493915d6c
tshegofatsoseane <tshegofatsoseane98@gmail.com>
2023-04-29 10:41:00 -0700

    Task 101, fix

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,15 +1,11 @@
 section .data
-    message db "Hello, Holberton", 0
-    newline db 10, 0
 
 	global   main
 	  extern    printf
 main:
 	  mov   edi, format
 	  xor   eax, eax
-	add rsp, 8
-	xor eax, eax
-	  call  printf
+	call  printf
 	  mov   eax, 0
 	  ret
 format: db `Hello, Holberton\n`,0
