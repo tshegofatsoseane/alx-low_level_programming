f721bced3da991e0938e7a30cd7fa889bb8690fe
tshegofatsoseane <tshegofatsoseane98@gmail.com>
2023-04-29 14:43:21 -0700

    Fix, Task 4

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,22 +1,23 @@
 #include "lists.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
- * free_list - a function that frees a list_t list.
+ * free_list - a function that releases a list_t list.
  * @head: the list
  */
 
 void free_list(list_t *head)
 {
-	list_t *temp;
+	list_t *tempo;
 	int count;
 
 	while (head)
 	{
-		temp = head->next;
+		tempo = head->next;
 		free(head->str);
 		free(head);
-		head = temp;
+		head = tempo;
 		count++;
 	}
 }
