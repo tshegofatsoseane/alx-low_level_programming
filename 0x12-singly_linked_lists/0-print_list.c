ac7ac1f9531fd405382bcdd1ab82310f2f9afefc
tshegofatsoseane <tshegofatsoseane98@gmail.com>
2023-04-28 19:46:16 -0700

    Task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * print_list - displays a singly linked list
+ * @h: Head of the linked list
+ * Return: umber of nodes
+ */
+
+/** Author: Tshegofatso Seane */
+
+size_t print_list(const list_t *h)
+{
+
+	size_t count = 0;
+	int counter;
+
+	while (h != NULL)
+	if (h->str == NULL)
+	{
+		printf("[%d] (nil)\n", h->len);
+	}
+	else
+	{
+		printf("[%d] %s\n", h->len, h->str);
+	}
+
+	count++;
+	h = h->next;
+	return (count);
+}
