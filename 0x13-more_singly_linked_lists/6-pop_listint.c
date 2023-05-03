26b8fd616d3d52fafead523bac382c1c5c4c84e5
tshegofatsoseane <tshegofatsoseane98@gmail.com>
2023-05-03 10:32:21 -0700

    Task 6

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * pop_listint - a function that deletes the head nod
+ * @head: pointe
+ * Return: Data
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int num;
+
+	if (!head || !*head)
+	{
+		return (0);
+	}
+
+	num = (*head)->n;
+	temp = (*head)->next;
+	free(*head);
+	*head = temp;
+
+	return (num);
+}
