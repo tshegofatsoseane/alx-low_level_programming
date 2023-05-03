5ae211c375b579f5aa67687c01958f3b26b464b3
tshegofatsoseane <tshegofatsoseane98@gmail.com>
2023-05-03 12:11:22 -0700

    Update

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,14 +10,14 @@
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	unsigned int i = 0;
-	listint_t *temp = head;
+	unsigned int count = 0;
+	listint_t *tempor = head;
 
-	while (temp && i < index)
+	while (tempor && count < index)
 	{
-		temp = temp->next;
-		i++;
+		tempor = tempor->next;
+		count++;
 	}
 
-	return (temp ? temp : NULL);
+	return (tempor ? tempor : NULL);
 }
