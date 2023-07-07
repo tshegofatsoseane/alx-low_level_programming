30cdaae1f110165290782d1065d1ea32ace4ea68
tshegofatsoseane <tshegofatsoseane98@gmail.com>
2023-07-06 18:34:56 -0700

    First task

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * binary_to_uint - a new function that converts binary num to unsigned
+ * @b: binary number
+ * Return: results
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int j;
+	unsigned int decimal_val = 0;
+
+	if (!b)
+	{
+		return (0);
+	}
+
+	for (j = 0; b[j]; j++)
+	{
+		if (b[j] < '0' || b[j] > '1')
+		{
+			return (0);
+		}
+		decimal_val = 2 * decimal_val + (b[j] - '0');
+	}
+
+return (decimal_val);
+}
