885e2eb261943656065f90c95e4a1ef5134ecbfa
tshegofatsoseane <tshegofatsoseane98@gmail.com>
2023-08-17 06:57:54 -0700

    First files

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - a funct that gives number of elements
+ * @h: lst head
+ * Return: Results
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	int count;
+
+	count = 0;
+
+	if (h == NULL)
+		return (count);
+
+	while (h->prev != NULL)
+		h = h->prev;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
