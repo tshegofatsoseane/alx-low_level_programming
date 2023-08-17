77e75e7cfeb5d39274a8f435aff2dd412e5dd2b0
tshegofatsoseane <tshegofatsoseane98@gmail.com>
2023-08-17 07:52:06 -0700

    Last files

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - a function to calculate sum
+ * @head: list head
+ * Return: results
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum;
+
+	sum = 0;
+
+	if (head != NULL)
+	{
+		while (head->prev != NULL)
+			head = head->prev;
+
+		while (head != NULL)
+		{
+			sum += head->n;
+			head = head->next;
+		}
+	}
+
+	return (sum);
+}
